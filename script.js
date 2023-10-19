f5a8dbb05621734d1de85e1cd5d26faa75bac448
Telhadoi <ivan08mail.com@icloud.com>
2023-10-19 17:26:16 +0300

    sdffas
    asdfasfda

---
+++ script.js
@@ -0,0 +1,11 @@
+let menu = document.querySelector('.fixed-menu');
+
+menu.addEventListener('click', function(event){
+    let clickTarget = event.target;
+    let activeBtn = document.querySelector('.active');
+
+    if(clickTarget.classList.contains('nav-link')){
+        clickTarget.classList.add('active');
+        activeBtn.classList.remove('active');
+    }
+});
